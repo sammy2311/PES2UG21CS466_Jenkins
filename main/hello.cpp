ec07129a0ef281eccb4ad5800cc131d600f8f0a1
king z <samarthingalagavi@gmail.com~>
2024-03-11 21:24:10 +0530

    Task 4 complete

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run by PES2UG21CS466\n";
+  cout << "I have successfully built and run by PES2UG21CS466 also known by Samarth S I\n";
   return 0;
 }
